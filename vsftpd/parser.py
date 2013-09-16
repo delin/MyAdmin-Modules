fd2d7eed100350486f37467a8bfdc841a60f7b9a
Yulin Denis <delin@delin.pro>
2013-09-16 18:13:35 +0400

    0.02

--- vsftpd/parser.py
+++ vsftpd/parser.py
@@ -1,10 +1,9 @@
-import re
-
 __author__ = 'delin'
 
+import re
 from ConfigParser import RawConfigParser
 from StringIO import StringIO
-from MyAdmin.functions import get_system_users
+from MyAdmin.functions import get_system_users, system_cmd, replace_in_file
 
 
 class vsFTPdParser(object):
@@ -12,50 +11,152 @@ class vsFTPdParser(object):
     default_config = "/etc/vsftpd.conf"
     ftp_users_file = "/etc/ftp_users.conf"
     vsftpd_version = 3.0
+    fd = None
+    fp = None
 
     default_values = {
-        "anonymous_enable": "YES",
-        "local_enable": "YES",
-        "write_enable": "YES",
-        "local_umask": "022",
-        "anon_upload_enable": "YES",
-        "anon_mkdir_write_enable": "YES",
-        "dirmessage_enable": "YES",
-        "xferlog_enable": "YES",
-        "connect_from_port_20": "YES",
-        "chown_uploads": "YES",
-        "chown_username": "whoever",
-        "xferlog_file": "/var/log/vsftpd.log",
-        "xferlog_std_format": "YES",
-        "idle_session_timeout": "600",
-        "data_connection_timeout": "120",
-        "nopriv_user": "ftpsecure",
-        "async_abor_enable": "YES",
-        "ascii_upload_enable": "YES",
-        "ascii_download_enable": "YES",
-        "ftpd_banner": "Welcome to blah FTP service.",
-        "deny_email_enable": "YES",
-        "banned_email_file": "/etc/vsftpd.banned_emails",
-        "chroot_local_user": "YES",
-        "chroot_list_enable": "YES",
-        "chroot_list_file": "/etc/vsftpd.chroot_list",
-        "ls_recurse_enable": "YES",
-        "listen": "YES",
-        "listen_ipv6": "YES",
+        "anonymous_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "local_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "write_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "local_umask": {
+            "default": "022",
+            "re": "[0-7]{3}",
+        },
+        "anon_upload_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "anon_mkdir_write_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "dirmessage_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "xferlog_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "connect_from_port_20": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "chown_uploads": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "chown_username": {
+            "default": "whoever",
+            "re": "[\w]{1,128}",
+        },
+        "xferlog_file": {
+            "default": "/var/log/vsftpd.log",
+            "re": "^[\W+\/\\\]{1,}",
+        },
+        "xferlog_std_format": {
+            "default": "YES",
+            "type": "bool",
+            "re": "^YES$|^NO$",
+        },
+        "idle_session_timeout": {
+            "default": "600",
+            "re": "[\d+]{1,6}",
+        },
+        "data_connection_timeout": {
+            "default": "120",
+            "re": "[\d+]{1,6}",
+        },
+        "nopriv_user": {
+            "default": "ftpsecure",
+            "re": "[\w]{1,128}",
+        },
+        "async_abor_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "ascii_upload_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "ascii_download_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "ftpd_banner": {
+            "default": "Welcome to blah FTP service.",
+            "re": "[\S+]{0,}",
+        },
+        "deny_email_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "banned_email_file": {
+            "default": "/etc/vsftpd.banned_emails",
+            "re": "^[\W+\/\\\]{1,}",
+        },
+        "chroot_local_user": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "chroot_list_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "chroot_list_file": {
+            "default": "/etc/vsftpd.chroot_list",
+            "re": "^[\W+\/\\\]{1,}",
+        },
+        "ls_recurse_enable": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "listen": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
+        "listen_ipv6": {
+            "default": "YES",
+            "re": "^YES$|^NO$",
+        },
     }
 
     def __init__(self, conf_path=default_config):
-        conf_text = '[' + self.section + ']\n' + open(conf_path, 'r').read()
-        conf_fp = StringIO(conf_text)
+        conf_text = '[' + self.section + ']\n' + open(conf_path, 'rw').read()
+        self.fp = StringIO(conf_text)
+        # self.fp = open(conf_path, 'rw')
 
         self.config = RawConfigParser()
-        self.config.readfp(conf_fp)
+        self.config.readfp(self.fp)
+
+    def check_value(self, option, value):
+        if re.match(self.default_values[option]['re'], value):
+            return True
+        return False
+
+    def get_default_value(self, option):
+        value = self.default_values[option]['default'].copy() or None
+        return value
 
     def get(self, option):
         if self.config.has_option(self.section, option):
             return self.config.get(self.section, option)
-        else:
-            return None
+        return None
+
+    def set(self, option, value):
+        if self.check_value(option, value):
+            self.config.set(self.section, option, value)
+            return True
+        return False
 
     def options(self):
         return self.config.options(self.section)
@@ -78,3 +179,24 @@ class vsFTPdParser(object):
                 ftp_users.append(user)
 
         return ftp_users
+
+    def get_configs_array(self):
+        configs = self.default_values.copy()
+
+        for option in configs:
+            configs[option] = {
+                "value": self.get(option),
+                "default": self.default_values[option]['default'] or None,
+            }
+        return configs
+
+    def write(self):
+        items = self.config.items(self.section)
+        for (key, value) in items:
+            grep_string = "grep '^%s=*' %s" % (key, self.default_config)
+            if system_cmd(grep_string) == 0:
+                print "replace", key, value
+                replace_in_file(self.default_config, r"%s=(.*)" % key, '%s=%s' % (key, value))
+            else:
+                add_string = "echo '%s=%s' >> %s" % (key, value, self.default_config)
+                system_cmd(add_string)
--- vsftpd/views.py
+++ vsftpd/views.py
@@ -1,20 +1,33 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 from django.utils.translation import ugettext as _
-from MyAdmin.functions import prepare_data, get_system_users
+from MyAdmin.functions import prepare_data, service_reload
 from modules.vsftpd.parser import vsFTPdParser
 
 
 @login_required
-@require_http_methods(["GET"])
+@require_http_methods(["GET", "POST"])
 def main(request):
     page_title = _("Info")
 
     config = vsFTPdParser()
     options = config.options()
     users = config.get_ftp_users()
+
+    c = {}
+    c.update(csrf(request))
+
+    if 'POST' in request.method:
+        if 'btn-reload' in request.POST:
+            if service_reload("vsftpd", request):
+                messages.success(request,  _("Service reloaded"))
+            else:
+                messages.error(request,  _("Service reloaded error"))
+
+        return redirect('module-vsftpd_main')
 
     config_options = []
     for option in options:
@@ -33,46 +46,59 @@ def main(request):
 
 
 @login_required
-@require_http_methods(["GET"])
+@require_http_methods(["GET", "POST"])
 def edit(request):
     page_title = _("Edit")
 
-    config = vsFTPdParser()
-    options = config.options()
+    c = {}
+    c.update(csrf(request))
 
-    config_options = []
-    for option in options:
-        config_options.append({
-            "name": option,
-            "value": config.get(option),
-            "default": config.default_values.get(option),
-        })
+    config = vsFTPdParser()
+
+    if request.method == "POST":
+        if request.POST.get('anonymous_enable'):
+            config.set('anonymous_enable', 'YES')
+        else:
+            config.set('anonymous_enable', 'NO')
+
+        if request.POST.get('anon_upload_enable'):
+            config.set('anon_upload_enable', 'YES')
+        else:
+            config.set('anon_upload_enable', 'NO')
+
+        if request.POST.get('anon_mkdir_write_enable'):
+            config.set('anon_mkdir_write_enable', 'YES')
+        else:
+            config.set('anon_mkdir_write_enable', 'NO')
+
+        if request.POST.get('local_enable'):
+            config.set('local_enable', 'YES')
+        else:
+            config.set('local_enable', 'NO')
+
+        if request.POST.get('write_enable'):
+            config.set('write_enable', 'YES')
+        else:
+            config.set('write_enable', 'NO')
+
+        if request.POST.get('listen'):
+            config.set('listen', 'YES')
+        else:
+            config.set('listen', 'NO')
+
+        if request.POST.get('listen_ipv6'):
+            config.set('listen_ipv6', 'YES')
+        else:
+            config.set('listen_ipv6', 'NO')
+
+        if request.POST.get('local_umask'):
+            config.set('local_umask', request.POST.get('local_umask'))
+
+        config.write()
+        return redirect('module-vsftpd_main')
 
     return render(request, "edit.html", {
         'page_title': page_title,
         'data': prepare_data(request),
-        'config_options': config_options,
-    })
-
-
-@login_required
-@require_http_methods(["GET", "POST"])
-def user_add(request):
-    page_title = _("Edit")
-
-    config = vsFTPdParser()
-    options = config.options()
-
-    config_options = []
-    for option in options:
-        config_options.append({
-            "name": option,
-            "value": config.get(option),
-            "default": config.default_values.get(option),
-        })
-
-    return render(request, "user_add.html", {
-        'page_title': page_title,
-        'data': prepare_data(request),
-        'config_options': config_options,
+        'configs': config.get_configs_array(),
     })
